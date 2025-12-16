c8c4900cc0322d18ae8a45ac2df62982b6832b8d
javierespitia83 <javierespitia83@gmail.com>
2025-12-16 18:17:48 -0500

    Menu_Todo_list

--- todo_list.py
+++ todo_list.py
@@ -1,2 +1,9 @@
 #This is a To Do List project by @javierespitia
-print("to do list")
+def display_menu():
+    """Display menu."""
+    print("--To Do List Menu --")
+    print("1. Add task")
+    print("2. View task")
+    print("1. Mark task as completed")
+    print("1. View")
+    print("--------")
